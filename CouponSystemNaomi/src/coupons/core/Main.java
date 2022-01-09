d586a51c369c9987024ffffe51caa471f8a357b4
jbt <jbt@40X-XX>
2022-01-09 16:20:10 +0200

    09-01-22

--- CouponSystemNaomi/src/coupons/core/Main.java
+++ CouponSystemNaomi/src/coupons/core/Main.java
@@ -62,7 +62,7 @@ public class Main {
 			System.out.println("all: " + couponsDBDao.getAllCoupons());
 			
 //			couponsDBDao.addCouponPurchase(1, 2);
-			couponsDBDao.deleteCouponPurchase(1, 2);
+//			couponsDBDao.deleteCouponPurchase(1, 2);
 
 		} catch (CouponSystemException e) {
 			System.out.println("EROR: " + e.getMessage());
