6dc61d32797c02d5b1cc86426d93f6a0ed1275b0
lbar5 <lbar5@LAPTOP-R1UTE1V5>
2022-02-01 16:05:59 +0200

    springWeb -01/02/22

--- I-SpringWeb2/src/main/java/app/core/controllers/BookController.java
+++ I-SpringWeb2/src/main/java/app/core/controllers/BookController.java
@@ -1,9 +1,16 @@
 package app.core.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,15 +25,50 @@ public class BookController {
 	@Autowired
 	private BookService bookService;
 	
-	//http://localhost:8080/books?bookId=125 -> request parameter
-	//http://localhost:8080/books/125 -> path variable
-	@GetMapping(path = "/{bookId}")
-	public Book getOneBook(@PathVariable int bookId) {
-		return this.bookService.getOneBook(bookId);
+	@PostMapping(consumes =  {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
+	public ResponseEntity<?> addBook(@RequestBody Book book) {
+		try {
+			return ResponseEntity.ok("book's id: " + this.bookService.addBook(book));
+		}catch (Exception e) {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+		}
 	}
 	
-	@PostMapping
-	public int addBook(@RequestBody Book book) {
-		return this.bookService.addBook(book);
+	//http://localhost:8080/books?bookId=125 -> request parameter
+	//http://localhost:8080/books/125 -> path variable
+//	@GetMapping(path = "/{bookId}", produces = {"application/json", "application/xml"})
+	@GetMapping(path = "/{bookId}", produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
+	public ResponseEntity<?> getOneBook(@PathVariable int bookId) {
+		try {
+			Book book = this.bookService.getOneBook(bookId);
+			return new ResponseEntity<>(book, HttpStatus.OK);
+		}catch (Exception e) {
+			return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
+		}
+	}
+	
+	@GetMapping(produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
+	public List<Book> getAllBooks(){
+		return this.bookService.getAllBooks();
+	}
+	
+	@PutMapping(consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
+	public ResponseEntity<?> updateBook(@RequestBody Book book){
+		try {
+			this.bookService.updateBook(book);
+			return ResponseEntity.ok("book updated: " + book);
+		}catch (Exception e) {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+		}
+	}
+	
+	@DeleteMapping(path = "/{bookId}")
+	public ResponseEntity<?> deleteBook(@PathVariable int bookId){
+		try {
+			this.bookService.deleteBook(bookId);
+			return ResponseEntity.ok("book id " + bookId + " deleted");
+		} catch (Exception e) {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+		}
 	}
 }
--- I-SpringWeb2/src/main/java/app/core/service/BookService.java
+++ I-SpringWeb2/src/main/java/app/core/service/BookService.java
@@ -1,5 +1,6 @@
 package app.core.service;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,6 +18,9 @@ public class BookService {
 	private BookRepo bookRepo;
 	
 	public int addBook(Book book) {
+		if(this.bookRepo.existsById(book.getId())) {
+			throw new RuntimeException("addBook failed - book id " + book.getId() + " already exists");			
+		}
 		return this.bookRepo.save(book).getId();
 	}
 	
@@ -25,7 +29,27 @@ public class BookService {
 		if(opt.isPresent()) {
 			return opt.get();
 		}
-		throw new RuntimeException("getOneBook failed - book id " + bookId + " not found");
+		throw new RuntimeException("getOneBook failed - book id " + bookId + " NOT found");
+	}
+	
+	public List<Book> getAllBooks(){
+		return this.bookRepo.findAll();
+	}
+	
+	public void updateBook(Book book) {
+		if(this.bookRepo.existsById(book.getId())) {
+			this.bookRepo.save(book); //update
+		}else {
+			throw new RuntimeException("updateBook failed - book id " + book.getId() + " NOT found");
+		}
+	}
+	
+	public void deleteBook(int bookId) {
+		if(this.bookRepo.existsById(bookId)) {
+			this.bookRepo.deleteById(bookId);
+		}else {
+			throw new RuntimeException("deleteBook failed - book id " + bookId + " NOT found");			
+		}
 	}
 	
 }
